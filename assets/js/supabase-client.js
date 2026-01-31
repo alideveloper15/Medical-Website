3cc3502d6c22dea8d76ac9de1e06a6d423875f35
alideveloper15 <172058104+alideveloper15@users.noreply.github.com>
2026-02-01 03:04:43 +0500

    Added supabsae package and client

---
+++ assets/js/supabase-client.js
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://jmtcaldstjvnaxgojkkr.supabase.co",
+  "sb_publishable_OHE3jRwu2msedg5wATP7bg_Hmj8MstZ"
+)
+
+// async function loginUser({email, password}) {
+//   const {data, error} = await supabase
+//     .auth
+//     .signInWithPassword({email, password})
+// }
+
+// async function getData() {
+//   const {data, error} = await supabase
+//     .from()
+//     .select()
+//     .eq()
+
+// }
